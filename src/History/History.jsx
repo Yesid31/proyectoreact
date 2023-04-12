6cbc0743b12f8201cc8f20f8973e50c8a0980be8
andrea <antivarandrea>
2023-04-12 08:52:42 -0500

    todo

---
+++ src/History/History.jsx
@@ -0,0 +1,32 @@
+//import 'History.css'
+export function  History (){
+    return (
+        <>
+
+        <div className='container'>
+            <div className='row'>
+                <div className='col-12 col-md-3'>
+                    <h2>Grupo Firme</h2>
+                    <p>Grupo Firme es una agrupación de música regional mexicana creada por Jairo Corrales y Eduin Cazares en la ciudad de Tijuana, Baja California en 2014.</p>
+                    <img src="https://firebasestorage.googleapis.com/v0/b/musicosygt.appspot.com/o/foto1.jpg?alt=media&token=6116d1cb-089f-4175-803f-4968bcf3ec96" alt="foto" className='img-fluid w-100'/>
+                </div>
+
+                <div className='col-12 col-md-6'>
+                    <h3>Historia Agrupacion</h3>
+                    <p>La agrupación fue creada en 2014 en la ciudad de Tijuana, México, por Eduin Cazares y Jairo Corrales. Su primer álbum llamado Pasado, Presente, Futuro salió en el año 2017 con un total de doce canciones. El Barco y En Vivo Desde Tijuana: los Buitrones y los Firmes fueron sus álbumes posteriores.</p>
+                    <img src="https://firebasestorage.googleapis.com/v0/b/musicosygt.appspot.com/o/foto2.jpg?alt=media&token=87281a23-bc6b-4db3-bc57-6205af6bab60" alt="foto2" className="img-fluid-w100" />
+                </div>
+
+                <div className='col-12 col-md-3'>
+                    <p>En 2020 lanzaron los sencillos «Pídeme», «El Roto» y «Juro Por Dios». Posteriormente realizaron una presentación musical en Colombia, lanzado simultáneamente un álbum llamando En Vivo desde Medellín, Colombia.</p>
+                    <img src="https://firebasestorage.googleapis.com/v0/b/musicosygt.appspot.com/o/foto3%20.jpg?alt=media&token=a8fda7bd-bd87-41df-8ef7-c12fbefd0653" alt="foto3" className="img-fluid-w100"/>
+
+                    <p>En 2021 ganaron cuatro Premios Juventud,y cinco premios, de seis nominaciones, de los Premios Lo Nuestro,El 18 de noviembre ganaron su primer Grammy Latino en la categoría de Mejor Álbum de Música Banda por su álbum Nos Divertimos Logrando Lo Imposible.</p>
+                </div>
+            </div>
+
+        </div>
+
+        </>
+    )
+}
